36ccde3703750ef91ea1734ac0ea3c1c944291fd
zhuxiaojt <lhz131019@hotmail.com>
2026-07-19 07:18:52 +0800

    fix: 0.4.3: Add default value for Application options

--- src/Application.ts
+++ src/Application.ts
@@ -19,18 +19,18 @@ class Application {
     windows: WindowPool;
     logger: Logger;
     utils: __Utils;
-    constructor(options: { WindowsWebview2UserDataFolder?: string, LogFilePath?: string }) {
+    constructor(options: { WindowsWebview2UserDataFolder?: string, LogFilePath?: string } = {}) {
         this.app = new WebviewApplication();
         this.utils = new __Utils(this.app);
         this.logger = new Logger(options.LogFilePath);
-        const poolOptions = {WindowsWebview2UserDataFolder: options.WindowsWebview2UserDataFolder, logger: this.logger};
+        const poolOptions = { WindowsWebview2UserDataFolder: options.WindowsWebview2UserDataFolder, logger: this.logger };
         this.windows = new WindowPool(this.app, poolOptions);
         this.app.whenReady();
     }
     createTray(options: { id: string, tooltip: string, icon: TrayIconImage | undefined, menuItems: Array<{ id: string, label: string }> }) {
         return new Tray(this.app, options);
     }
-    exit(){
+    exit() {
         this.app.exit();
     }
 }
