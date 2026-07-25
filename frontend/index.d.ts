fe37b325c59e72adb05a8cba856e0eafe754c734
zhuxiaojt <lhz131019@hotmail.com>
2026-07-25 16:55:18 +0800

    feat: 0.5.2: Add frontend type declarations for window methods

---
+++ frontend/index.d.ts
@@ -0,0 +1,21 @@
+export {}
+
+declare global {
+  interface Window {
+    tell: (to: number, message: string, data?: any) => void
+    broadcast: (message: string, data?: any) => void
+    invoke: (event: string, data?: any) => Promise<any>
+    close: () => void
+    minimize: () => void
+    unminimize: () => void
+    maximize: () => void
+    unmaximize: () => void
+    setTitle: (title: string) => void
+    openDevTools: () => void
+    closeDevTools: () => void
+    setGlobal: (name: string, value: any) => void
+    getGlobal: (name: string) => Promise<any>
+    id: number
+    isNexfepLoadDone: boolean
+  }
+}
